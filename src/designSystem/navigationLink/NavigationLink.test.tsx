31e5a293ffe9dfeaaf4c4d5ab7b4f377ef67d1c8
khorh <helen.khor@googlemail.com>
2023-06-04 09:00:35 +0100

    added navigation link test

---
+++ src/designSystem/navigationLink/NavigationLink.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import NavigationLink from "./NavigationLink";
+
+describe("navigation link", () => {
+  test("should render a navigation link", () => {
+    render(<NavigationLink name={"Link"} destination={"#link"} />);
+    const navigationLinkElement = screen.getByRole("link");
+    expect(navigationLinkElement.tagName).toBe("A");
+    expect(navigationLinkElement).toHaveClass("navigationLink");
+    expect(navigationLinkElement.textContent).toBe("Link");
+    expect(navigationLinkElement).toHaveAttribute("href", "#link");
+  });
+});
