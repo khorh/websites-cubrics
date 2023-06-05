d58a8b9d80d3aa3c01e7cfaedbe4944f40344069
khorh <helen.khor@googlemail.com>
2023-06-05 10:17:20 +0100

    updated with web accessibility and unit testing comments

--- src/designSystem/icon/Icon.tsx
+++ src/designSystem/icon/Icon.tsx
@@ -1,6 +1,16 @@
 import { FC } from "react";
 import Menu from "./list.svg";
 
+/* 
+  Web accessibility
+  - Role is not required as the <img /> tag infers the role.
+  - Aria label is not required as the alt attribute infers the label.
+  - For more information, see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/img_role.
+
+  Unit testing
+  - The unique alt attribute is to be used to determine if the icon is present in other components e.g. navigation bar.
+*/
+
 const Icon: FC = () => {
   return <img src={Menu} alt="menu icon" />;
 };
--- src/designSystem/logo/Logo.tsx
+++ src/designSystem/logo/Logo.tsx
@@ -1,6 +1,16 @@
 import { FC } from "react";
 import LogoCubrics from "./logo-cubrics.svg";
 
+/* 
+  Web accessibility
+  - Role is not required as the <img /> tag infers the role.
+  - Aria label is not required as the alt attribute infers the label.
+  - For more information, see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/img_role.
+
+  Unit testing
+  - The unique alt attribute is to be used to determine if the logo is present in other components e.g. navigation bar.
+*/
+
 const Logo: FC = () => {
   return <img src={LogoCubrics} alt="cubrics logo" />;
 };
