2c3cec8c0a1d35fa37b4605573e639f3f1908f89
khorh <helen.khor@googlemail.com>
2023-06-12 13:45:03 +0100

    added purpose component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { FC } from "react";
 import styles from "./App.module.scss";
 import NavigationBar from "./designSystem/navigationBar/NavigationBar";
 import Header from "./components/header/Header";
+import Purpose from "./components/purpose/Purpose";
 
 const App: FC = () => {
   return (
     <div className={styles.page}>
       <NavigationBar />
       <Header />
+      <Purpose />
     </div>
   );
 };
---
+++ src/components/purpose/Purpose.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+import styles from "./Purpose.module.scss";
+import Heading from "../../designSystem/heading/Heading";
+import Text from "../../designSystem/text/Text";
+
+const Purpose: FC = () => {
+  return (
+    <section className={styles.common} aria-label={"purpose"}>
+      <Heading type={"h2"} colour={"primary__dark"}>
+        PURPOSE
+      </Heading>
+      <Text type={"p1"}>
+        Our purpose is to realise our client's business value by delivering high
+        quality results, at a faster pace and lowering your transformation
+        costs.
+      </Text>
+    </section>
+  );
+};
+
+export default Purpose;
