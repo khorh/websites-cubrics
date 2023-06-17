3027f729f93b689492cc197d778605cbd0c555ae
khorh <helen.khor@googlemail.com>
2023-06-17 16:27:23 +0100

    changed div to section

--- src/components/industries/Industries.tsx
+++ src/components/industries/Industries.tsx
@@ -6,14 +6,14 @@ import { industriesMockedData } from "./Industries.mocked";
 
 const Industries: FC = () => {
   return (
-    <div className={styles.common} aria-label="industries">
+    <section className={styles.common} aria-label="industries">
       <div className={styles.title} aria-label="title">
         <Heading type={"h2"} colour={"primary__dark"}>
           INDUSTRIES
         </Heading>
       </div>
       <CardIndustryGroup data={industriesMockedData} />
-    </div>
+    </section>
   );
 };
 
