013d81a8fb4a0ef376d4ede63b23d6ce677faa89
khorh <helen.khor@googlemail.com>
2023-06-18 08:00:03 +0100

    added test for about group

--- src/designSystem/card/cardAboutGroup/CardAboutGroup.tsx
+++ src/designSystem/card/cardAboutGroup/CardAboutGroup.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import styles from "./CardAboutGroup.module.scss";
 import CardAbout from "../cardAbout/CardAbout";
 
-interface ICardAboutGroup {
+interface ICardAboutGroup extends React.HTMLAttributes<HTMLTextAreaElement> {
   data: {
     about_name: string;
     about_description: string;
@@ -14,7 +14,7 @@ const CardAboutGroup: FC<ICardAboutGroup> = (props) => {
     return <CardAbout data={about} key={about.about_name} />;
   });
   return (
-    <article className={styles.common} aria-label={"about-content"}>
+    <article className={styles.container} aria-label={"about-content"}>
       {displayAboutData}
     </article>
   );
