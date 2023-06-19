d9529514f0f5da77177b7f16690f0fa6c37aeca8
khorh <helen.khor@googlemail.com>
2023-06-19 10:35:55 +0100

    updated styling naming and tests for footer

--- src/components/footer/Footer.test.tsx
+++ src/components/footer/Footer.test.tsx
@@ -5,6 +5,13 @@ describe("footer", () => {
   test("should render a footer", () => {
     render(<Footer />);
     const footerElement = screen.getByRole("contentinfo");
+    expect(footerElement.tagName).toBe("FOOTER");
+    expect(footerElement).toHaveClass("container", { exact: true });
+  });
+  test("should render a footer content", () => {
+    render(<Footer />);
+    const footerElement = screen.getByText("© Copyright Cubrics 2023");
+    expect(footerElement.tagName).toBe("P");
     expect(footerElement.textContent).toBe("© Copyright Cubrics 2023");
   });
 });
