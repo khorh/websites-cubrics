43fc26808b31637fde3527929145ea0094d71856
khorh <helen.khor@googlemail.com>
2023-06-19 22:38:18 +0100

    updated code

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -6,13 +6,17 @@ import { serviceMockedData } from "./Services.mocked";
 
 const Services: FC = () => {
   return (
-    <section id={"services"} className={styles.container} aria-label="services-section">
-      <article className={styles.title} aria-label="services-title">
+    <section
+      id={"services"}
+      className={styles.container}
+      aria-label={"services-section"}
+    >
+      <article className={styles.title} aria-label={"services-title"}>
         <Heading type={"h2"} colour={"primary__dark"}>
           SERVICES
         </Heading>
       </article>
-        <CardServiceGroup data={serviceMockedData} />
+      <CardServiceGroup data={serviceMockedData} />
     </section>
   );
 };
