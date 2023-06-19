2aa1408911974c3326d9c52595713c73cdc70872
khorh <helen.khor@googlemail.com>
2023-06-19 07:46:45 +0100

    removed comments

--- src/designSystem/icon/Icon.tsx
+++ src/designSystem/icon/Icon.tsx
@@ -1,14 +1,5 @@
 import { FC } from "react";
 import { iconMockedImages } from "./Icon.mocked";
-/* 
-  Web accessibility
-  - Role is not required as the <img /> tag infers the role.
-  - Aria label is not required as the alt attribute infers the label.
-  - For more information, see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/img_role.
-
-  Unit testing
-  - The unique alt attribute is to be used to determine if the icon is present in other components e.g. navigation bar.
-*/
 
 interface IIcon extends React.HTMLAttributes<HTMLImageElement> {
   name: string;
