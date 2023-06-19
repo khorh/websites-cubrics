ae1388690dc76dded9d4c2ed2f11239d576f767f
khorh <helen.khor@googlemail.com>
2023-06-19 07:43:31 +0100

    move comments to readme

--- src/designSystem/button/Button.tsx
+++ src/designSystem/button/Button.tsx
@@ -1,15 +1,6 @@
 import { FC } from "react";
 import styles from "./Button.module.scss";
 
-/* 
-  Web accessibility
-  - Role is not required as the <button> tag infers the role as button.
-  - For more information, see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/button_role.
-
-  Unit testing
-  - To access the <button> tag, use the role (getByRole).
-*/
-
 interface IButton extends React.HTMLAttributes<HTMLButtonElement> {}
 
 const Button: FC<IButton> = (props) => {
