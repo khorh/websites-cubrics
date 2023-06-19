2f032338ea325640bd1f7162ad1e7dbbabd9c578
khorh <helen.khor@googlemail.com>
2023-06-19 22:21:47 +0100

    added tests for services component

--- src/components/services/Services.mocked.tsx
+++ src/components/services/Services.mocked.tsx
@@ -1,30 +1,34 @@
 interface IServiceMockedData {
+  service_id: string;
   service_icon: string;
-  service_name: string;
+  service_title: string;
   service_intro: string;
   service_description: string;
 }
 
 export const serviceMockedData: IServiceMockedData[] = [
   {
+    service_id: "project management",
     service_icon: "file post",
-    service_name: "Project Management",
+    service_title: "Project Management",
     service_intro:
       "Companies are continuously transforming their organization to stay competitive.",
     service_description:
       "At Cubrics, our structured methodologies equip you to plan and execute your projects by leveraging our knowledge, tools and expertise gained from consulting with blue-chip brands.",
   },
   {
+    service_id: "transformational change",
     service_icon: "hurricane",
-    service_name: "Transformational Change",
+    service_title: "Transformational Change",
     service_intro:
       "Technology alone is not sufficient to drive change.  Change needs to be embedded in the company's culture.",
     service_description:
       "At Cubrics, we drive these changes throughout the value chain by addressing the capability gap in employees and utilizing our modern communication techniques to embed changes for your suppliers and customers.",
   },
   {
+    service_id: "effective workshops",
     service_icon: "people",
-    service_name: "Effective Workshops",
+    service_title: "Effective Workshops",
     service_intro:
       "Workshops address some of the top key issues faced when companies embark on project work.",
     service_description:
--- src/components/services/Services.test.tsx
+++ src/components/services/Services.test.tsx
@@ -5,13 +5,25 @@ describe("service", () => {
   test("should render the services section", () => {
     render(<Services />);
     const servicesElement = screen.getByLabelText("services-section");
-    expect(servicesElement).toHaveClass("common", { exact: true });
-    const titleElement = screen.getByLabelText("title");
-    expect(titleElement).toHaveClass("title", { exact: true });
-    expect(titleElement.textContent).toBe("SERVICES");
-    const serviceGroupElement = screen.getByLabelText("serviceGroup");
-    expect(serviceGroupElement).toHaveClass("serviceGroup", { exact: true });
-    expect(serviceGroupElement.textContent).toBe(
+    expect(servicesElement.tagName).toBe("SECTION");
+    expect(servicesElement.id).toBe("services");
+    expect(servicesElement).toHaveClass("container", { exact: true });
+  });
+  test("should render the services' title", () => {
+    render(<Services />);
+    const servicesTitleElement = screen.getByLabelText("services-title");
+    expect(servicesTitleElement.tagName).toBe("ARTICLE");
+    expect(servicesTitleElement).toHaveClass("title");
+    const servicesTitleHeadingElement = screen.getByRole("heading", {
+      level: 2,
+    });
+    expect(servicesTitleHeadingElement.tagName).toBe("H2");
+    expect(servicesTitleHeadingElement.textContent).toBe("SERVICES");
+  });
+  test("should render the services' content", () => {
+    render(<Services />);
+    const servicesContentElement = screen.getByLabelText("services-content");
+    expect(servicesContentElement.textContent).toBe(
       "Project ManagementCompanies are continuously transforming their organization to stay competitive.At Cubrics, our structured methodologies equip you to plan and execute your projects by leveraging our knowledge, tools and expertise gained from consulting with blue-chip brands.Transformational ChangeTechnology alone is not sufficient to drive change.  Change needs to be embedded in the company's culture.At Cubrics, we drive these changes throughout the value chain by addressing the capability gap in employees and utilizing our modern communication techniques to embed changes for your suppliers and customers.Effective WorkshopsWorkshops address some of the top key issues faced when companies embark on project work.At Cubrics, we explore the requirements for a successful delivery which is tailored to your company's needs and engaging with the relevant stakeholders."
     );
   });
