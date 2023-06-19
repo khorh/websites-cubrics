eaf56df376616f01cf80699dc869d08805759810
khorh <helen.khor@googlemail.com>
2023-06-19 22:44:03 +0100

    updated test for card about

--- src/designSystem/card/cardAbout/CardAbout.test.tsx
+++ src/designSystem/card/cardAbout/CardAbout.test.tsx
@@ -9,27 +9,19 @@ const purposeData = {
 };
 
 describe("card about", () => {
-  test("should render a card about", () => {
+  test("should render the card about", () => {
     render(<CardAbout data={purposeData} />);
-    const cardAboutElement = screen.getByRole("generic", {
-      name: /about-content-individual/i,
-    });
+    const cardAboutElement = screen.getByLabelText("about-content-individual");
     expect(cardAboutElement.tagName).toBe("DIV");
     expect(cardAboutElement).toHaveClass("container");
   });
-  test("should render a card about title", () => {
-    const purposeData = {
-      about_id: "purpose",
-      about_title: "Purpose",
-      about_description:
-        "Our purpose is to realise our client's business value by delivering high quality results, at a faster pace and lowering your transformation costs.",
-    };
+  test("should render the card about's title", () => {
     render(<CardAbout data={purposeData} />);
     const cardAboutTitleElement = screen.getByRole("heading", { level: 3 });
     expect(cardAboutTitleElement.tagName).toBe("H3");
     expect(cardAboutTitleElement.textContent).toBe("Purpose");
   });
-  test("should render a card about description", () => {
+  test("should render the card about's description", () => {
     render(<CardAbout data={purposeData} />);
     const cardAboutDescriptionElement = screen.getByText(
       "Our purpose is to realise our client's business value by delivering high quality results, at a faster pace and lowering your transformation costs."
