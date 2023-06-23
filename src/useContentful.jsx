2e8d2d9ab47e08201dea8583304aed4b3b7cdef4
khorh <helen.khor@googlemail.com>
2023-06-23 13:02:00 +0100

    added env file to protect API keys

--- src/useContentful.jsx
+++ src/useContentful.jsx
@@ -2,5 +2,5 @@ import { createClient } from "contentful";
 
 export const client = createClient({
   space: "8ux9r9ey3jdc",
-  accessToken: "bTSiXLv4ZOkPMylFxdP02rW6e2_Co1dXOkU1N2mxq0k",
+  accessToken: process.env.REACT_APP_API_KEY,
 });
