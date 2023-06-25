1147b6c76074509b6a169692e9f4458edcb4b832
khorh <helen.khor@googlemail.com>
2023-06-25 08:18:06 +0100

    created utils for making title uppercase

---
+++ src/utils/title.utils.ts
@@ -0,0 +1,10 @@
+/**
+ * Method to format the title to uppercase
+ * @param title which is a string
+ * @returns
+ */
+
+export const titleUppercase = (title: string) => {
+  const changeTitle = title.toLocaleUpperCase();
+  return changeTitle;
+};
