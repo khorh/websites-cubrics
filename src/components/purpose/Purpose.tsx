99031d34e8fb46a7c68d3395d7a45b4c464608cd
khorh <helen.khor@googlemail.com>
2023-06-25 08:06:10 +0100

    updated purpose with contentful

--- src/components/purpose/Purpose.tsx
+++ src/components/purpose/Purpose.tsx
@@ -1,21 +1,51 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import styles from "./Purpose.module.scss";
 import Heading from "../../designSystem/heading/Heading";
 import Text from "../../designSystem/text/Text";
+import { client } from "../../useContentful";
+
+interface IPurposeResponse {
+  title: string;
+  description: string;
+}
 
 const Purpose: FC = () => {
-  return (
-    <section className={styles.container} aria-label={"purpose-section"}>
-      <Heading type={"h2"} colour={"primary__dark"}>
-        PURPOSE
-      </Heading>
-      <Text type={"p1"}>
-        Our purpose is to realise our client's business value by delivering high
-        quality results, at a faster pace and lowering your transformation
-        costs.
-      </Text>
-    </section>
-  );
+  const [purpose, setPurpose] = useState<IPurposeResponse[]>([]);
+
+  const getPurpose = async () => {
+    try {
+      const entries = await client.getEntries({
+        content_type: "purpose",
+      });
+      const sanitizedEntries = entries.items.map((item) => {
+        return { ...item.fields };
+      });
+      return sanitizedEntries;
+    } catch (error) {
+      console.log(`Error fetching purpose ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    getPurpose().then((response: any) => setPurpose(response));
+  });
+
+  const displayPurposeContent = purpose.map((item: IPurposeResponse) => {
+    return (
+      <section
+        className={styles.container}
+        aria-label={"purpose-section"}
+        key={item.title}
+      >
+        <Heading type={"h2"} colour={"primary__dark"}>
+          {item.title}
+        </Heading>
+        <Text type={"p1"}>{item.description}</Text>
+      </section>
+    );
+  });
+
+  return <>{displayPurposeContent}</>;
 };
 
 export default Purpose;
