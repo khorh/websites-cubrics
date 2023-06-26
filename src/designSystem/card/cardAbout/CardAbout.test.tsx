f6ad549b8c7e4e997a63de5042121e367d857b1e
khorh <helen.khor@googlemail.com>
2023-06-26 07:13:44 +0100

    updated card about tests

--- src/designSystem/card/cardAbout/CardAbout.test.tsx
+++ src/designSystem/card/cardAbout/CardAbout.test.tsx
@@ -1,35 +1,33 @@
 import { render, screen } from "@testing-library/react";
 import CardAbout from "./CardAbout";
-
-const aboutMockedDataId1 = {
-  id: 1,
-  title: "Purpose",
-  description:
-    "Our purpose is to realise our client's business value by delivering high quality results, at a faster pace and lowering your transformation costs.",
-  order: 1,
-};
+import { aboutMockedData } from "../../../components/about/About.mocked";
 
 describe("card about", () => {
-  test("should render the card about", () => {
-    render(<CardAbout data={aboutMockedDataId1} />);
-    const cardAboutElement = screen.getByLabelText("about-content-individual");
-    expect(cardAboutElement.tagName).toBe("DIV");
-    expect(cardAboutElement).toHaveClass("container", { exact: true });
+  const aboutMockedDataTests = aboutMockedData.map((item) => {
+    test("should render the card about", () => {
+      render(<CardAbout data={item} />);
+      const cardAboutElement = screen.getByLabelText(
+        "about-content-individual"
+      );
+      expect(cardAboutElement.tagName).toBe("DIV");
+      expect(cardAboutElement).toHaveClass("container", { exact: true });
+    });
+    test("should render the card about's title", () => {
+      render(<CardAbout data={item} />);
+      const cardAboutTitleElement = screen.getByRole("heading", { level: 3 });
+      expect(cardAboutTitleElement.tagName).toBe("H3");
+      expect(cardAboutTitleElement.textContent).toBe(`${item.title}`);
+    });
+    test("should render the card about's description", () => {
+      render(<CardAbout data={item} />);
+      const cardAboutDescriptionElement = screen.getByText(
+        `${item.description}`
+      );
+      expect(cardAboutDescriptionElement.tagName).toBe("P");
+      expect(cardAboutDescriptionElement.textContent).toBeInTheDocument;
+    });
   });
-  test("should render the card about's title", () => {
-    render(<CardAbout data={aboutMockedDataId1} />);
-    const cardAboutTitleElement = screen.getByRole("heading", { level: 3 });
-    expect(cardAboutTitleElement.tagName).toBe("H3");
-    expect(cardAboutTitleElement.textContent).toBe("Purpose");
-  });
-  test("should render the card about's description", () => {
-    render(<CardAbout data={aboutMockedDataId1} />);
-    const cardAboutDescriptionElement = screen.getByText(
-      "Our purpose is to realise our client's business value by delivering high quality results, at a faster pace and lowering your transformation costs."
-    );
-    expect(cardAboutDescriptionElement.tagName).toBe("P");
-    expect(cardAboutDescriptionElement.textContent).toBe(
-      "Our purpose is to realise our client's business value by delivering high quality results, at a faster pace and lowering your transformation costs."
-    );
-  });
+  {
+    aboutMockedDataTests;
+  }
 });
