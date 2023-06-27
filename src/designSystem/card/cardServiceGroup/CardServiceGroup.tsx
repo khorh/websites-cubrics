adb81b0d8a7820457b932c5ada58ee1b0e204e48
khorh <helen.khor@googlemail.com>
2023-06-27 08:15:43 +0100

    added sort into services

--- src/designSystem/card/cardServiceGroup/CardServiceGroup.tsx
+++ src/designSystem/card/cardServiceGroup/CardServiceGroup.tsx
@@ -8,9 +8,12 @@ interface ICardServiceGroup extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 const CardServiceGroup: FC<ICardServiceGroup> = (props) => {
-  const displayServicesData = props.data.map((service) => {
+  const sortServicesData = props.data.sort((a, b) => a.order - b.order);
+
+  const displayServicesData = sortServicesData.map((service) => {
     return <CardService data={service} key={service.id} />;
   });
+
   return (
     <article className={styles.container} aria-label={"services-content"}>
       {displayServicesData}
