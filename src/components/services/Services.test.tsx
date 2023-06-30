2da61fb60728d52ae38766f8f0b75bd93c0bace7
khorh <helen.khor@googlemail.com>
2023-06-30 06:19:09 +0100

    resolved failed tests

--- src/components/services/Services.test.tsx
+++ src/components/services/Services.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Services from "./Services";
 
 describe("service", () => {
@@ -20,11 +20,13 @@ describe("service", () => {
     expect(servicesTitleHeadingElement.tagName).toBe("H2");
     expect(servicesTitleHeadingElement.textContent).toBe("SERVICES");
   });
-  test("should render the services' content", () => {
+  test("should render the services' content", async () => {
     render(<Services />);
-    const servicesContentElement = screen.getByLabelText("services-content");
-    expect(servicesContentElement.textContent).toBe(
-      "Project ManagementCompanies are continuously transforming their organization to stay competitive.At Cubrics, our structured methodologies equip you to plan and execute your projects by leveraging our knowledge, tools and expertise gained from consulting with blue-chip brands.Transformational ChangeTechnology alone is not sufficient to drive change.  Change needs to be embedded in the company's culture.At Cubrics, we drive these changes throughout the value chain by addressing the capability gap in employees and utilizing our modern communication techniques to embed changes for your suppliers and customers.Effective WorkshopsWorkshops address some of the top key issues faced when companies embark on project work.At Cubrics, we explore the requirements for a successful delivery which is tailored to your company's needs and engaging with the relevant stakeholders."
-    );
+    await waitFor(() => {
+      const servicesContentElement = screen.getByLabelText("services-content");
+      expect(servicesContentElement.textContent).toBe(
+        "Project ManagementCompanies are continuously transforming their organization to stay competitive.At Cubrics, our structured methodologies equip you to plan and execute your projects by leveraging our knowledge, tools and expertise gained from consulting with blue-chip brands.Transformational ChangeTechnology alone is not sufficient to drive change. Change needs to be embedded in the company's culture.At Cubrics, we drive these changes throughout the value chain by addressing the capability gap in employees and utilizing our modern communication techniques to embed changes for your suppliers and customers.Effective WorkshopsWorkshops address some of the top key issues faced when companies embark on project work.At Cubrics, we explore the requirements for a successful delivery which is tailored to your company's needs and engaging with the relevant stakeholders."
+      );
+    });
   });
 });
